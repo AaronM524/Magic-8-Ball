7cb59ce1b5b645ea39800c074c498354cf333213
AaronM524 <aaronmathew468@gmail.com>
2023-12-19 21:31:30 -0500

    finished

--- script.js
+++ script.js
@@ -1,62 +1,77 @@
-function shake8Ball(){
-   const question = prompt('Ask the Magic 8-Ball a question:');
-   if (question === null) {
-       document.getElementById('response-text').innerText = 'Please ask a question!';
-       document.getElementById('response-text').classList = 'text-danger';
-       document.getElementById('response-image').src = '#BADIMAGE';
-       return
-   }
-   if (!question.trim()) {
-       alert('Please enter a valid question!');
-       return
-   }
-   const randNum = Math.floor(Math.random()*8);
-   switch(randNum) {
-       case 0:
-           answer = "Yes";
-           image = "#YESIMAGE";
-           color = "text-success";
-           break;
-       case 1:
-           answer = "No";
-           image = "#NOIMAGE";
-           color = "text-success";
-           break;
-       case 2:
-           answer = "Ask again later";
-           image = "#LATERIMAGE";
-           color = "text-success";
-           break;
-       case 3:
-           answer = "Maybe";
-           image = "#MAYBEIMAGE";
-           color = "text-success";
-           break;
-       case 4:
-           answer = "Cannot predict now";
-           image = "#CANTIMAGE";
-           color = "text-success";
-           break;
-       case 5:
-           answer = "Don't count on it";
-           image = "#UNSUREIMAGE";
-           color = "text-success";
-           break;
-       case 6:
-           answer = "Most likely";
-           image = "#LIKELYIMAGE";
-           color = "text-success";
-           break;
-       case 7:
-           answer = "Outlook not so good";
-           image = "#NOTLIKELYIMAGE";
-           color = "text-danger";
-           break;
-       default:
-           break;
-   }
-   const responseText = document.getElementById('response-text');
-   responseText.innerText = answer;
-   responseText.classList = `display-4 ${color}`;
-   document.getElementById('response-text').src = image;
+function shakeMagic8Ball() {
+    const question = prompt(`Ask the Magic 8-ball a question:`);
+    if (question === null) {
+        document.getElementById('response-text').innerText = `MUST ASK A QUESTION`
+        document.getElementById('response-text').classList = `lead text-danger`
+        document.getElementById('response-image').src = "images/Angry-Man.webp"
+    }
+    if (!question.trim()) {
+        alert(`Enter a Valid Question`)
+        return
+    }
+
+    const randomNumber = Math.floor(Math.random() * 8);
+    let answer, image, color;
+
+
+    switch (randomNumber) {
+        case 0:
+            answer = "Yes"
+            image = "images/440px-Thumbs_up_icon.svg.png"
+            color = "text-success"
+            break
+
+        case 1:
+            answer = "No"
+            image = "images/pngtree-finger-cartoon-hand-showing-thumbs-down-png-image_6231374.png"
+            color = "text-danger"
+            break
+
+        case 2:
+            answer = "Reply hazy try again"
+            image = "images/I-dont-know.jpg"
+            color = "text-warning"
+            break
+
+        case 3:
+            answer = "It is certain"
+            image = "images/How-to-be-confident.jpg"
+            color = "text-success"
+            break
+
+        case 3:
+            answer = "Cannot predict now"
+            image = "images/2061563-Stephen-Hawking-Quote-You-cannot-predict-the-future.jpg"
+            color = "text-warning"
+            break
+
+        case 4:
+            answer = "Do not count on it"
+            image = "images/1_ASwOspasPrML_zbtPyOhJw.jpg"
+            color = "text-danger"
+            break
+
+        case 5:
+            answer = "My sources say no"
+            image = "images/download (1).png"
+            color = "text-danger"
+            break
+
+        case 6:
+            answer = "Signs point to yes"
+            image = "images/download (5).jpg"
+            color = "text-success"
+            break
+
+        case 7:
+            answer = "Outlook not so good"
+            image = "images/1610370371_16483.jpg"
+            color = "text-danger"
+            break
+    }
+    const responseText = document.getElementById(`response-text`)
+    responseText.innerText = answer;
+    responseText.classList = `display-4 ${color}`;
+    document.getElementById(`response-image`).src = image;
+
 }
